00077918445dd0896516364c27f5da625118976e
DiegoCerda98 <diego.act98@gmail.com>
2020-07-31 15:47:48 -0500

    sfsd

--- src/database.js
+++ src/database.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://test:<1978>@cluster0-shard-00-00.g2iri.mongodb.net:27017,cluster0-shard-00-01.g2iri.mongodb.net:27017,cluster0-shard-00-02.g2iri.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-x2qgib-shard-0&authSource=admin&retryWrites=true&w=majority', {
-    useNewUrlParser: true
+mongoose.connect('mongodb://localhost/chat', {    useNewUrlParser: true
 })
   .then(db => console.log('db connected'))
   .catch(err => console.log(err));
